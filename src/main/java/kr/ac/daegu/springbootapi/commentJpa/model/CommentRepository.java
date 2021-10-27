c10fcc076b21a5b5438a52a48fa2bff79a42b3f6
dwc04112 <dwc04112@gmail.com>
2021-10-27 18:58:21 +0900

    POST /commentjpa/ 복수 트랜잭션 처리 추가

---
+++ src/main/java/kr/ac/daegu/springbootapi/commentJpa/model/CommentRepository.java
@@ -0,0 +1,10 @@
+
+package kr.ac.daegu.springbootapi.commentJpa.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+}
