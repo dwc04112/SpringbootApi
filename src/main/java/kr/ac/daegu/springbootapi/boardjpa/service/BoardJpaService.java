0333be49d4b3b642d8f9c29793c23c67ebe1997b
dwc04112 <dwc04112@gmail.com>
2021-10-29 19:39:50 +0900

    Revert "POST /boardjpa/reply 추가"
    This reverts commit d20cd4763b94e1cb131e81de06e72c119fd0306a.

--- src/main/java/kr/ac/daegu/springbootapi/boardjpa/service/BoardJpaService.java
+++ src/main/java/kr/ac/daegu/springbootapi/boardjpa/service/BoardJpaService.java
@@ -1,4 +1,3 @@
-
 package kr.ac.daegu.springbootapi.boardjpa.service;
 
 import kr.ac.daegu.springbootapi.board.model.BoardDTO;
@@ -10,10 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -88,60 +87,5 @@ public class BoardJpaService {
         } else {
             return new ApiResponse(false, "failed to delete board id " + id);
         }
-    }
-
-    @Transactional
-    public BoardDTO postReply(BoardDTO dto) {
-        // JPQL TEST
-        Board b = boardRepository.selectBoard(dto.getReplyRootId());
-        log.debug(b.toString());
-
-        /* depth와 orderNum을 정하는 로직 */
-        int replyRootId = dto.getReplyRootId();
-        int depth = dto.getDepth();
-        int orderNum = dto.getOrderNum();
-
-        Integer minOrderNum = boardRepository.getMinOrderNum(replyRootId, depth, orderNum);
-        log.debug("minOrderNum==" + minOrderNum);
-        // minOrderNum이 0인 경우 : root글에 달린 답글들 사이에 추가되는 답글인지? 바로추가답글 : 사이답글임.
-        if(minOrderNum == 0) {
-            log.debug("======root글에 달린 답글들 사이에 추가되는 답글이 아님(바로추가답글)======");
-            orderNum = boardRepository.getReplyOrderNum(replyRootId);
-        } else {
-            log.debug("======root글에 달린 답글들 사이에 추가되는 답글.(사이답글)======");
-            boardRepository.updateOrderNum(replyRootId, minOrderNum);
-            orderNum = minOrderNum;
-        }
-
-        int newDepth = depth + 1;
-        String newSubject = appendPrefixString("RE : ", depth, dto.getSubject());
-
-        //dao.insertReplyContent(newId, subject, author, content, password, replyRootId, depth, orderNum);
-        b.setSubject(newSubject);
-        b.setAuthor(dto.getAuthor());
-        b.setContent(dto.getContent());
-        b.setPassword(dto.getPassword());
-        b.setReplyRootId(replyRootId);
-        b.setDepth(newDepth);
-        b.setOrderNum(orderNum);
-        boardRepository.save(b);
-
-        // JPA를 사용함에 있어 Entity 객체와 DTO객체를 일부러 분리한 이유, Entity <-> DTO의 변환 참고
-        // https://dbbymoon.tistory.com/4
-        dto.setSubject(newSubject);
-        dto.setReplyRootId(replyRootId);
-        dto.setDepth(newDepth);
-        dto.setOrderNum(orderNum);
-        return dto;
-    }
-
-    // 아래 메소드의 뜻을 한국어로 번역 해 보시오.
-    private String appendPrefixString(String appendPrefix, int loop, String target) {
-        StringBuilder builder = new StringBuilder();
-        for(int i=1; i<=loop; i++){
-            builder.append(appendPrefix);
-        }
-        builder.append(target);
-        return builder.toString();
     }
 }
